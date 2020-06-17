68993d5dd3939e6cd95d4c13f0a8b7f4bb01c612
Tanyon <66812813+tanyonb-orikaban@users.noreply.github.com>
2020-06-17 03:44:46 -0700

    Add files via upload

---
+++ mfg_nice.py
@@ -0,0 +1,59 @@
+"""
+[TBD], my game that will include a decent amount of randomness and selection,
+as well as novel encounters and maybe a loot system!
+Why not, this could be a time to showcase some creativity and create some
+interesting characters and interaction. I like games, games can be fun if you
+put some thought into them so why not. I'll try to make a fun game.
+"""
+
+
+# things to search for:
+#- function or perameter to allow one char to be missing from string (if statement?)
+#- random function types, what can i utilize here?
+#
+#
+#
+#
+
+################################# CHAR IDEAS ##################################
+## + perhaps a vendor or beneficial stranger that appears quite seldomly and
+## rewards the player
+##
+## + there's this black dude, right. so he's hanging out crouching japanese style
+## and he's looking pretty solemn and you really don't feel like you could even
+## be around him because his presence is so...BIG. but he's a medium sized dude,
+## just muscley as heck. reminds me of raiden from mortal kombat.
+##
+## + dude named bonadio. actually means good day in italian so perhaps only when
+## weather is right.
+##
+## + knowledgable cat character. obligatory feline race.
+
+
+
+# Player Class, it can either contain the variable for a health system or health
+# variable. Should have a gear and initial loadout method, as well as a function
+# for equipping.
+
+# Monster Class, can roll for species upon generation? Each species would have
+# it's own attributes (attr)
+
+# NPC Class, perhaps could incorporate disposition but that's a lot of coding.
+# Boy I'm already getting ambitious. Could be a rewarding side project however
+# and a good showcase of my potential work.
+
+
+
+# Wherever you start, I want it to be revisited at some point, maybe a fountain
+# whose liquid changes upon every encounter. Another thought was for this game
+# to be loopable, either before or after resetting. Could have persistent items
+# potentially.
+
+
+# name gen function, no reason why not. hell i could attempt to scrub name
+# databases based on user input if i wanted. i do need to get into that.
+
+
+# one of the conditions for this game in the book is that this code must be
+# self-contained, and in this case means all if must have else and air tight.
+# so how can i best work with those constraints?
