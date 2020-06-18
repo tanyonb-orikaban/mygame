597d3019fdae29033f14002832104848830c408a
tanyonb-orikaban <tanyon.banks@gmail.com>
2020-06-18 05:01:21 -0700

    - learning markup for the layout - added to most of the sections

---
+++ Docs/layout.md
@@ -0,0 +1,42 @@
+GAME PLAN (so to speak)
+
+Outline what the game looks like, what the implementations are on paper or sep
+document first. Right now I'm using comments inside the code itself which is
+acceptable but a proper outline is important.
+
+Order should be:
+
+  Outline -> Code -> Debug -> Code -> Finishing touches
+
+However, coding bit by bit and checking the working status I think is important
+and as soon as I have a general idea starting on that is not a bad idea.
+
+I'm using markdown, here's the run-down:
+- #### indicates heading for a room or instance
+- *()* italicized is an action, something to implement.
+- [] is just like () in english strings for thoughts.
+
+
+#### MAIN MENU
+An intro to the game with a start function and a command list that's basic?
+I want the player to be able to interact with the environment zork-style,
+research zork?
+
+*(list commands)*
+*(start game)* -
+*(initialize character)* - Starting with a character of your choice could be
+interesting, attributes would change based on race.
+
+#### DARK ROOM
+Starting in a dark room [underground?] you *(fumble around)* to find a ladder.
+Don't know how you got here, *(what's your name)*?
+*(Climb the ladder)*.
+
+#### COURTYARD
+On the surface you see a courtyard with a fountain and 4 doors, you are
+completely walled in.
+*(weather system - but it will be sunny everytime on the first loop)*
+There is daylight and it's sunny.
+*(door choice)* Which will you choose? [Should some be locked?]}
+
+#### DOOR 1
--- mfg_nice.py
+++ mfg_nice.py
@@ -6,14 +6,22 @@ interesting characters and interaction. I like games, games can be fun if you
 put some thought into them so why not. I'll try to make a fun game.
 """
 
+# import modules, will need os for sure
+import os
 
 # things to search for:
 #- function or perameter to allow one char to be missing from string (if statement?)
 #- random function types, what can i utilize here?
+#- how to lock the user terminal and set sizes (proper display of text w/o wrap)
 #
 #
 #
-#
+
+#---------------------------USEFUL COMMANDS-------------------------------------
+# os.get_terminal_size()
+
+
+
 
 ################################# CHAR IDEAS ##################################
 ## + perhaps a vendor or beneficial stranger that appears quite seldomly and
@@ -28,12 +36,41 @@ put some thought into them so why not. I'll try to make a fun game.
 ## weather is right.
 ##
 ## + knowledgable cat character. obligatory feline race.
+##
+## +
+##
+
+
+
+
+
+
+################################# PLAYER COMMANDS #############################
+## -- (command) and plain text is mostly unrecognized
+## -- perhaps having a connection between commands can work with if statements
+##
+## + (where) am i?
+##
+## + (what)-(is)-(object)? >>
+##
+## + (viewstats) >> realistic layout for nice viewing of stats
+##
+## +
+##
 
 
 
 # Player Class, it can either contain the variable for a health system or health
 # variable. Should have a gear and initial loadout method, as well as a function
 # for equipping.
+
+class Player(self, **kwargs): # I think
+    """It's you."""
+
+    def __init__(self, name, race, class):
+
+
+
 
 # Monster Class, can roll for species upon generation? Each species would have
 # it's own attributes (attr)
@@ -57,3 +94,10 @@ put some thought into them so why not. I'll try to make a fun game.
 # one of the conditions for this game in the book is that this code must be
 # self-contained, and in this case means all if must have else and air tight.
 # so how can i best work with those constraints?
+
+
+
+# COMBAT SYSTEM
+# using loops i would use a tick system. so buffs etc will reduce the number
+# to reach in order to perform an action in combat (damage).
+# for example, Player reaches an action at 1.5(ticks). Vagabond
